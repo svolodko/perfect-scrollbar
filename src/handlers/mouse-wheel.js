319323948ab9b1fae2e571c242ee45ed7ec08013
Serg Volodko <svolodko@gmail.com>
2019-04-08 12:01:49 +0300

    fix delta wheel

--- src/handlers/mouse-wheel.js
+++ src/handlers/mouse-wheel.js
@@ -51,6 +51,14 @@ export default function(i) {
       deltaY = e.wheelDelta;
     }
     console.log('DeltaX = %i, DeltaY = %i', deltaX, deltaY);
+    if (deltaX && deltaY) {
+      if (deltaX > deltaY) {
+        deltaY = 0;
+      } else {
+        deltaX = 0;
+      }
+    }
+    
     if (e.shiftKey) {
       // reverse axis with shift key
       return [-deltaY, -deltaX];
